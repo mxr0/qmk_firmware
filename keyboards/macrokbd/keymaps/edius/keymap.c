872f5def8033750e1aa831e6f598f0b323482185
Maximilian Ronniger <madhu1008@gmail.com>
2022-11-11 15:39:46 +0100

    kacrokbd: update edius keymap

--- keyboards/macrokbd/keymaps/edius/keymap.c
+++ keyboards/macrokbd/keymaps/edius/keymap.c
@@ -20,8 +20,7 @@ uint16_t edius[] = {
   KC_5,
   KC_6,
   KC_7,
-  KC_8,
-  KC_9
+  KC_0
 };
 
 int eid=0;
@@ -61,20 +60,20 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_EDIUS] = LAYOUT(
-        KC_F7, KC_F8, LALT(KC_ENT), TO(_EDIUS2),
-        KC_G, LALT(KC_G), LCTL(KC_T), LCTL(KC_P)
+        KC_N, KC_M, LCTL(KC_R), TO(_EDIUS2),
+        LT(_MISC, LCTL(KC_Z)), KC_C, KC_A, KC_S
     ),
     [_EDIUS2] =  LAYOUT(
-        KC_I, KC_O, LALT(KC_U), TO(_EDIUS3),
-        LCTL(S(KC_S)), LCTL(KC_C), LCTL(KC_V), LCTL(KC_R)
+        KC_I, KC_O, KC_V, TO(_EDIUS3),
+        KC_F7, KC_F8, LALT(KC_ENT), KC_DEL
     ),
     [_EDIUS3] = LAYOUT(
-        KC_SPACE, LCTL(KC_Z), KC_C,TO(_EDIUS),
-        KC_N, KC_M, KC_COMMA, KC_DOT
+        LCTL(KC_T), LCTL(KC_P), LT(_MISC, KC_G),TO(_EDIUS),
+        KC_COMMA, KC_DOT, KC_SPACE, LCTL(S(KC_S))
     ),
     [_MISC] = LAYOUT(
-        _______, _______, _______,_______,
-        _______, _______, _______,_______
+        _______, LALT(KC_G),_______, _______,
+        _______, LCTL(KC_Y), _______,_______
     )
 };
 
@@ -84,22 +83,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool encoder_update_user(uint8_t index, bool clockwise) {
 /* left encoder */
     if (index == 1) {
-        if(clockwise){
-          if(eid>=9){
-            eid=0;
-          }
-          else {
-            eid++;
-          }
-	  tap_code16( C(edius[eid]) );
-        } else {
-          if(eid<=0){
-            eid=9;
-          }
-          else {
-            eid--;
-          }
-	  tap_code16( C(edius[eid]) );
+	switch(get_highest_layer(layer_state)){
+           case 0: /* _EDIUS*/
+             tap_code(clockwise ? KC_LEFT : KC_RIGHT);
+             break;
+	   case 1: /* _EDIUS2 */
+           case 2: /* _EDIUS3 */
+             if(clockwise){
+               if(eid>=8){
+                 eid=0;
+               }
+               else {
+                 eid++;
+               }
+               tap_code16( C(edius[eid]) );
+             } else {
+               if(eid<=0){
+                 eid=8;
+               }
+               else {
+                 eid--;
+               }
+               tap_code16( C(edius[eid]) );
+             }
+             break;
+           case 3: /* _MISC */
+                tap_code(clockwise ? KC_MNXT : KC_MPRV);
+                break;
 	}
     }
     return true;
